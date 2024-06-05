55839f9a9727269e6f5c113ed3aa79a190131beb
GeorgeTim1998 <georgetim1998@gmail.com>
2024-06-05 09:37:16 +0400

    8

--- practice/8/main.go
+++ practice/8/main.go
@@ -8,10 +8,10 @@ import (
 func setBit(num int64, i int, value int) int64 {
 	if value == 1 {
 		// Устанавливаем i-й бит в 1
-		num |= (1 << i)
+		num |= (1 << i) // |= операция логическое или
 	} else if value == 0 {
 		// Устанавливаем i-й бит в 0
-		num &= ^(1 << i)
+		num &= ^(1 << i) // &= операция логическое и
 	} else {
 		fmt.Println("Value must be 0 or 1")
 	}
